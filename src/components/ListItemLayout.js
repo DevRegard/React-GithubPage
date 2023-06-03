8bc478aa0b9ca30cd1015eada478336d1bb3717e
DevRegard <utr160626@gmail.com>
2023-06-03 16:12:06 +0900

    Feature ListItemLayout 구현 (2)

---
+++ src/components/ListItemLayout.js
@@ -0,0 +1,18 @@
+import cx from "clsx"
+import styles from "./ListItemLayout.module.css"
+
+const ListItemLayout = ({ children, checked, onChangeCheckBox, className }) => {
+  return (
+    <div className={cx(styles.wrapper, className)}>
+      <input
+        type="checkbox"
+        className={styles.checkbox}
+        value={checked}
+        onChange={onChangeCheckBox}
+      />
+      {children}
+    </div>
+  )
+}
+
+export default ListItemLayout
